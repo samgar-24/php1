299fd71a5e9ede2ba1e4c45e585abcd6f9bcc449
samgar-24 <99552144+samgar-24@users.noreply.github.com>
2022-04-02 09:38:45 +0600

    Add files via upload

---
+++ innndex.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = 45;
+        $b = 25;
+        $c = $a-$b;
+        $d = 7;
+        $result = $c + $d;
+        echo($result);   ?>
+</body>
+</html>
